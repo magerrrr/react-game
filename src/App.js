4652ef08a5eff32d7d2d54b379defcaf8da8a9b8
Kirill Mager <hi.kirill.m@gmail.com>
2021-02-28 13:41:33 +0300

    feat: create 6 components, add Router, mapping routes to components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import React from "react";
+import { Switch, Route } from "react-router";
+import Header from "./components/Header";
+import Play from "./components/Play";
+import Game from "./components/Game";
+import Footer from "./components/Footer";
+import NotFound from "./components/NotFound";
 
-class App extends React.Component {
-  render() {
-    const { name } = this.props;
-    return (
-      <>
-        <h1>Hello {name}</h1>
-      </>
-    );
-  }
+function App() {
+  return (
+    <>
+      <div className="container">
+        <Header />
+        <Switch>
+          <Route exact path="/" component={Play} />
+          <Route path="/game" component={Game} />
+          <Route path="*" component={NotFound} />
+        </Switch>
+      </div>
+      <Footer />
+    </>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
 import "./styles.scss";
 
 const mountNode = document.getElementById("app");
-ReactDOM.render(<App name="Kirill" />, mountNode);
+ReactDOM.render(
+  <Router>
+    <App />
+  </Router>,
+  mountNode
+);
