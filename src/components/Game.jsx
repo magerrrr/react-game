3808937b5f94c971cc5e0566be5df2e8e5624924
Kirill Mager <hi.kirill.m@gmail.com>
2021-02-28 16:29:09 +0300

    feat: add ability to play again

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 const Game = ({ myChoice, score, setScore }) => {
   const [house, setHouse] = useState("");
@@ -49,6 +50,9 @@ const Game = ({ myChoice, score, setScore }) => {
       {playerWin === "win" && <h2>WOW! You Win</h2>}
       {playerWin === "lose" && <h2>Oops... You Lose</h2>}
       {playerWin === "draw" && <h2>Draw!</h2>}
+      <Link to="/" onClick={() => setHouse()}>
+        Play again
+      </Link>
     </div>
   );
 };
